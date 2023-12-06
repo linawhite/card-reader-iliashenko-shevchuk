1c07680a8b17e5b0721ab7ff5f9e8d94f1181a06
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-06 19:29:49 +0100

    Dark theme of NavBar

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
+  <h1>Hello world</h1>
 <nav class="navbar bg-dark border-bottom border-body navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
