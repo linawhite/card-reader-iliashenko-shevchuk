53fd9a32e1b3fa732c7a93e68f07094c1f796d79
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-14 00:59:16 +0100

    edited

--- handle_form.php
+++ handle_form.php
@@ -19,12 +19,14 @@
 
 <?php
     $inputData = $_POST["pin"];
-    
+    // $json = file_get_contents('data.json');
+    // $json_data = json_decode($json, true);
+    // if (in_array($_GET["chip"], $json_data["chips"])) {
     // Read the JSON data from data.json
-    $jsonData = file_get_contents('./data.json');
-    $data = json_decode($jsonData, true);
+    $json = file_get_contents('data.json');
+    $json_data = json_decode($json, true);
     // Compare the submitted PIN with the stored PIN as strings
-    if (in_array(inputData, $data['pins'])) {
+    if (in_array($_POST["pin"], $json_data["pins"])) {
         $response = ['success' => true];
     } else {
         $response = ['success' => false];
