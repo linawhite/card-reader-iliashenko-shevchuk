16cefe2a96f400c56ff00283f82ba75b955b4ddc
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-13 12:24:23 +0100

    add pin checking

---
+++ pin_check.php
@@ -0,0 +1,14 @@
+<?php
+    if (array_key_exists("pin", $_GET)) {
+        $json = file_get_contents('data.json');
+        $json_data = json_decode($json, true);
+        
+        if ($_GET["pin"] != $json_data["pin"] ) {
+            echo json_encode(array('success' => false));
+            return;
+
+        }
+      }
+    
+      echo json_encode(array('success' => true));
+?>
