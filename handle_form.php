ddc69bb61280626e4ec2faa6e77b59236d1fbca2
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-14 01:19:14 +0100

    returned old handler php

--- handle_form.php
+++ handle_form.php
@@ -1,32 +1,18 @@
 <?php
-    // Error handling for reading JSON file
-    $json = file_get_contents('data.json');
-    if ($json === false) {
-        die('Error reading JSON file');
-    }
-
-    // Error handling for decoding JSON
-    $json_data = json_decode($json, true);
-    if ($json_data === null) {
-        die('Error decoding JSON');
-    }
-
-    // Sanitize the PIN value from the POST request
-    $pin = isset($_POST["pin"]) ? htmlspecialchars($_POST["pin"]) : null;
-
-    // Check if the PIN is set and exists in the array
-    if (isset($pin)) {
-        if (in_array($pin, $json_data["pins"])) {
-            $response = ['success' => true];
-        } else {
-            $response = ['success' => false];
-        }
+    $inputData = $_POST["pin"];
+    
+    // Read the JSON data from data.json
+    $jsonData = file_get_contents('./data.json');
+    $data = json_decode($jsonData, true);
+    // Compare the submitted PIN with the stored PIN as strings
+    if ($inputData === $data['pin']) {
+        $response = ['success' => true];
     } else {
-        // Handle the case where the PIN is not set
-        $response = ['success' => false, 'error' => 'Pin not set'];
+        $response = ['success' => false];
     }
 
     // Send JSON response to the client
     header('Content-Type: application/json');
     echo json_encode($response);
-?>
+
+?> 
