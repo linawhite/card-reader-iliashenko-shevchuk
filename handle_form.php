f2da3a38d78f70c07a7c48f1ab304b00b4a25501
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-14 01:02:39 +0100

    edited

--- handle_form.php
+++ handle_form.php
@@ -1,22 +1,3 @@
-<!-- <?php
-    $inputData = $_POST["pin"];
-    
-    // Read the JSON data from data.json
-    $jsonData = file_get_contents('./data.json');
-    $data = json_decode($jsonData, true);
-    // Compare the submitted PIN with the stored PIN as strings
-    if ($inputData === $data['pin']) {
-        $response = ['success' => true];
-    } else {
-        $response = ['success' => false];
-    }
-
-    // Send JSON response to the client
-    header('Content-Type: application/json');
-    echo json_encode($response);
-
-?> -->
-
 <?php
     $inputData = $_POST["pin"];
     // $json = file_get_contents('data.json');
