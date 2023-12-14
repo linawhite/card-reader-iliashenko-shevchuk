8be6a10d0ad1aa895b2520105c7f8e26043cece9
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-14 03:01:46 +0100

    add handler for second form

--- save_user.php
+++ save_user.php
@@ -3,28 +3,20 @@
     
     if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
           
-        function get_data() { 
-            // "id": "1234567890",
-            // "name": "Lina",
-            // "surname": "Iliashenko",
-            // "age": "19",
-            // "age": "FEI"
+        function get_data() {
             $datae = array(); 
             $datae[] = array( 
                 'id' => $_POST['id'], 
                 'name' => $_POST['name'], 
                 'surname' => $_POST['surname'], 
                 'age' => $_POST['age'], 
-                'age' => $_POST['age'], 
+                'faculty' => $_POST['faculty'], 
             ); 
             return json_encode($datae); 
         } 
-          
-        $name = "users"; 
-        $file_name = $name . '.json'; 
        
         if(file_put_contents( 
-            "$file_name", get_data())) { 
+            "users.json", get_data())) { 
                 echo $file_name .' file created'; 
             } 
         else { 
