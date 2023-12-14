f2210ad6ae1cfe8e6cbfe7abcd37340d5d3bb865
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-14 01:06:14 +0100

    edited

--- handle_form.php
+++ handle_form.php
@@ -1,12 +1,7 @@
 <?php
-    $inputData = $_POST["pin"];
-    // $json = file_get_contents('data.json');
-    // $json_data = json_decode($json, true);
-    // if (in_array($_GET["chip"], $json_data["chips"])) {
-    // Read the JSON data from data.json
     $json = file_get_contents('data.json');
     $json_data = json_decode($json, true);
-    // Compare the submitted PIN with the stored PIN as strings
+
     if (in_array($_POST["pin"], $json_data["pins"])) {
         $response = ['success' => true];
     } else {
@@ -16,5 +11,4 @@
     // Send JSON response to the client
     header('Content-Type: application/json');
     echo json_encode($response);
-
 ?>
