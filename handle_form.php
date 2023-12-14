9685a1322bbe7a11f7992032ea1bbe7123783ec1
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-14 01:10:41 +0100

    new handler

--- handle_form.php
+++ handle_form.php
@@ -1,11 +1,29 @@
 <?php
+    // Error handling for reading JSON file
     $json = file_get_contents('data.json');
-    $json_data = json_decode($json, true);
+    if ($json === false) {
+        die('Error reading JSON file');
+    }
 
-    if (in_array($_POST["pin"], $json_data["pins"])) {
-        $response = ['success' => true];
+    // Error handling for decoding JSON
+    $json_data = json_decode($json, true);
+    if ($json_data === null) {
+        die('Error decoding JSON');
+    }
+
+    // Sanitize the PIN value from the POST request
+    $pin = isset($_POST["pin"]) ? htmlspecialchars($_POST["pin"]) : null;
+
+    // Check if the PIN is set and exists in the array
+    if (isset($pin)) {
+        if (in_array($pin, $json_data["pins"])) {
+            $response = ['success' => true];
+        } else {
+            $response = ['success' => false];
+        }
     } else {
-        $response = ['success' => false];
+        // Handle the case where the PIN is not set
+        $response = ['success' => false, 'error' => 'Pin not set'];
     }
 
     // Send JSON response to the client
