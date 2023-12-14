bb7d92ee7b1247e1238e78e859ab53f3e847583b
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-14 03:50:41 +0100

    add handler for second form

--- handle_form.php
+++ handle_form.php
@@ -14,19 +14,19 @@
 
     echo '<script>console.log(' . json_encode($ids) . ');</script>';
 
-    // Compare the submitted PIN with the stored PIN as strings
-    if ($inputData === $data['pin']) {
-        $response = ['success' => true];
-    } else if (in_array($inputData, $ids)) {
-        // If inputData is found in the ids array
-        $response = ['success' => true];
-    } else {
-        // If inputData is not found in the ids array and it's not equal to the stored pin
-        $response = ['success' => false];
-    }
+    // // Compare the submitted PIN with the stored PIN as strings
+    // if ($inputData === $data['pin']) {
+    //     $response = ['success' => true];
+    // } else if (in_array($inputData, $ids)) {
+    //     // If inputData is found in the ids array
+    //     $response = ['success' => true];
+    // } else {
+    //     // If inputData is not found in the ids array and it's not equal to the stored pin
+    //     $response = ['success' => false];
+    // }
 
-    // Send JSON response to the client
-    header('Content-Type: application/json');
-    echo json_encode($response);
+    // // Send JSON response to the client
+    // header('Content-Type: application/json');
+    // echo json_encode($response);
 
 ?>
