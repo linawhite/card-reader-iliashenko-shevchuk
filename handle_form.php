3a4d8a24d4a3a1401258c85895d40e791bf64f07
Ліна Ільяшенко <lina.iliashenko@MacBook-Air-Lina.local>
2023-12-14 03:40:00 +0100

    add handler for second form

--- handle_form.php
+++ handle_form.php
@@ -1,13 +1,25 @@
 <?php
     $inputData = $_POST["pin"];
-    
+
     // Read the JSON data from data.json
     $jsonData = file_get_contents('./data.json');
     $data = json_decode($jsonData, true);
+
+    $ids = [];
+
+    // Loop through the users and collect their IDs
+    foreach ($data['users'] as $user) {
+        $ids[] = $user['id'];
+    }
+
     // Compare the submitted PIN with the stored PIN as strings
     if ($inputData === $data['pin']) {
         $response = ['success' => true];
+    } else if (in_array($inputData, $ids)) {
+        // If inputData is found in the ids array
+        $response = ['success' => true, 'message' => 'ID found'];
     } else {
+        // If inputData is not found in the ids array and it's not equal to the stored pin
         $response = ['success' => false];
     }
 
@@ -15,4 +27,4 @@
     header('Content-Type: application/json');
     echo json_encode($response);
 
-?> 
+?>
